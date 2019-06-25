feee558e2fc72fbc1a267b7b60eab93fc2ed0b27
Pecherskiy88 <pecherskiy88@gmail.com>
2019-06-25 11:48:41 +0300

    second commit

--- src/containers/Sidebar/UserProfile.js
+++ src/containers/Sidebar/UserProfile.js
@@ -1,34 +1,42 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {Avatar, Popover} from "antd";
-import {userSignOut} from "appRedux/actions/Auth";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Avatar, Popover } from "antd";
+import { userSignOut } from "appRedux/actions/Auth";
 
 class UserProfile extends Component {
-
   render() {
     const userMenuOptions = (
       <ul className="gx-user-popover">
         <li>My Account</li>
         <li>Connections</li>
-        <li onClick={() => this.props.userSignOut()}>Logout
-        </li>
+        <li onClick={() => this.props.userSignOut()}>Logout</li>
       </ul>
     );
 
     return (
-
       <div className="gx-flex-row gx-align-items-center gx-mb-4 gx-avatar-row">
-        <Popover placement="bottomRight" content={userMenuOptions} trigger="click">
-          <Avatar src='https://via.placeholder.com/150x150'
-                  className="gx-size-40 gx-pointer gx-mr-3" alt=""/>
-          <span className="gx-avatar-name">Rob Farnandies<i
-            className="icon icon-chevron-down gx-fs-xxs gx-ml-2"/></span>
+        <Popover
+          placement="bottomRight"
+          content={userMenuOptions}
+          trigger="click"
+        >
+          <Avatar
+            // src="https://via.placeholder.com/150x150"
+            src="https://images.pexels.com/photos/87611/sun-fireball-solar-flare-sunlight-87611.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+            className="gx-size-40 gx-pointer gx-mr-3"
+            alt="avatar"
+          />
+          <span className="gx-avatar-name">
+            Pecherskiy
+            <i className="icon icon-chevron-down gx-fs-xxs gx-ml-2" />
+          </span>
         </Popover>
       </div>
-
-    )
-
+    );
   }
 }
 
-export default connect(null, {userSignOut})(UserProfile);
+export default connect(
+  null,
+  { userSignOut }
+)(UserProfile);
